75d38ad952b7f1365df56c52734f9fef1a0268ce
Takuya Kanazawa <Takuya@Takuya-no-MacBook-Air.local>
2019-05-26 17:24:37 +0900

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-   vendor: ['axios'],
+   //vendor: ['axios'],
     extend(config, { isDev }) {
       if (isDev && process.client) {
         config.module.rules.push({
--- nuxtdown.config.js
+++ nuxtdown.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  mode: 'hash',
+
   api: function(isStatic) {
     const baseURL = "http://localhost:3000";
     const browserBaseURL = !isStatic ? "" : process.env.BASE_URL;
